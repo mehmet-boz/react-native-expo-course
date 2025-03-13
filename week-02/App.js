64425eb58a197ad1bbdcdf46d6d4dbe40ee14e2a
Student <mehmet-boz@hotmail.com.tr>
2025-03-13 13:01:40 +0300

    instagram header and local & network image

--- week-02/App.js
+++ week-02/App.js
@@ -1,48 +1,73 @@
 import { StatusBar } from "expo-status-bar";
-import { StyleSheet, Text, View } from "react-native";
+import { Image, SafeAreaView, StyleSheet, Text, View } from "react-native";
+import Feather from "@expo/vector-icons/Feather";
+import Constants from "expo-constants";
 
 export default function App() {
+  const img = require("./assets/images/user.jpg");
+  const webImage = { uri: "https://randomuser.me/api/portraits/women/19.jpg" };
   return (
     <View style={styles.container}>
-      <View style={styles.box}>
-        <Text>1</Text>
-        <Text>1</Text>
-      </View>
-      <View style={styles.box2}>
-        <Text>2</Text>
-      </View>
-      <View style={styles.box}>
-        <Text>3</Text>
-        <Text>3</Text>
-        <Text>3</Text>
-      </View>
-      <View style={styles.box3}>
-        <Text>4</Text>
-      </View>
+      <SafeAreaView>
+        <View style={styles.header}>
+          <View style={styles.headerLeft}>
+            <Feather name="camera" size={24} color="#262626" />
+          </View>
+          <Text style={styles.logoText}>Instagram</Text>
+          <View style={styles.headerRight}>
+            <Feather name="airplay" size={24} color="#262626" />
+            <Feather name="send" size={24} color="#262626" />
+          </View>
+        </View>
+        <View style={styles.content}>
+          <Image source={img} />
+          <Image source={img} />
+          <Image style={styles.image} source={webImage} />
+        </View>
+      </SafeAreaView>
     </View>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
-    backgroundColor: "#a5a1ff",
     flex: 1,
+    flexDirection: "column",
+    backgroundColor: "#fafafa",
+    alignItems: "stretch",
+    paddingTop: Constants.statusBarHeight,
+  },
+  header: {
     flexDirection: "row",
-    justifyContent: "space-evenly", //yatayda merkez
-    alignItems: "flex-start", //dikeyde sol
+    height: 48,
+    justifyContent: "space-between",
+    backgroundColor: "#fafafa",
+    alignItems: "center",
+    paddingHorizontal: 20,
+    borderBottomColor: "#A6A6AAaa",
+    borderBottomWidth: 1,
   },
-  box: {
-    width: 50,
-    backgroundColor: "#f55",
-    alignSelf: "stretch",
+  content: {
+    backgroundColor: "#fff",
+    flex: 1,
   },
-  box2: {
-    width: 75,
-    backgroundColor: "#f55",
-    alignSelf: "center",
+  headerRight: {
+    flexDirection: "row",
+    columnGap: 10,
+    flexBasis: 80,
+    justifyContent: "flex-end",
   },
-  box3: {
-    width: 20,
-    backgroundColor: "#f55",
+  headerLeft: {
+    flexBasis: 80,
+  },
+  logoText: {
+    fontSize: 24,
+    color: "#262626",
+    flex: 1,
+    textAlign: "center",
+  },
+  image: {
+    width: 150,
+    height: 150,
   },
 });
